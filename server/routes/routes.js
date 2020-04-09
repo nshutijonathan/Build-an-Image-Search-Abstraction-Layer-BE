015aeb15988851bd1e38228480c40c84202c5ece
nshutijonathan <nshutijonathan130@gmail.com>
2020-04-09 11:53:18 +0200

    added-search-functionality-172240018

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,5 +3,7 @@ import Images from '../controllers/images';
 const router = express();
 router.get('/api/v1/images', Images.AllImages);
 router.get('/api/v1/images/:id', Images.oneImage);
+router.get('/api/v1/search/images', Images.searchImages);
 router.post('/api/v1/images/add', Images.addImage);
+
 export default router;
