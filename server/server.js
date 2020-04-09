38a56f9597a63c52a196ed138cd5208359b736e0
nshutijonathan <nshutijonathan130@gmail.com>
2020-04-09 16:05:00 +0200

    added-dotenv

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import router from './routes/routes';
+import dotenv from 'dotenv';
 const app = express();
 app.use(express.json());
 app.use(router);
+dotenv.config();
 app.get('/', (req, res) => {
   return res.status(200).send({
     status: 200,
