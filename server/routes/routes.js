ad5dd7db1e58165d065cc0c4724981e06f5a239e
nshutijonathan <nshutijonathan130@gmail.com>
2020-04-09 14:54:15 +0200

    added-get-recent-search-strings-functionality-172242233

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,6 +4,6 @@ const router = express();
 router.get('/api/v1/images', Images.AllImages);
 router.get('/api/v1/images/:id', Images.oneImage);
 router.get('/api/v1/search/images', Images.searchImages);
+router.get('/api/v1/images/latest/imagesearch', Images.recentSearchString);
 router.post('/api/v1/images/add', Images.addImage);
-
 export default router;
